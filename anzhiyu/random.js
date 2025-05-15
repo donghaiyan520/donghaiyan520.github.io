9a2c56bd1ad2bbfd5afff28844ff2c276a400782
donghaiyan520 <xinjieming85@outlook.com>
2025-05-15 16:42:42 +0800

    Site updated: 2025-05-15 16:42:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/15/hello-world/","2025/05/15/GitKey 配置文件管理工具/"];function toRandomPost(){
+var posts=["2025/05/15/GitKey 配置文件管理工具/","2025/05/15/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
